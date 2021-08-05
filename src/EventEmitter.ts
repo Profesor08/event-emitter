ff0b6e7c0bb2eb2a14cf0dbb3bad4accf828674e
unknown <online7890@gmail.com>
2021-08-05 14:10:35 +0300

    created required configs

---
+++ src/EventEmitter.ts
@@ -0,0 +1,36 @@
+export class EventEmitter<EventType extends string, EventData extends unknown> {
+  protected events: {
+    [key in EventType]?: ((data: EventData) => void)[];
+  } = {};
+
+  private attachEvent(event: EventType, callback: (data: EventData) => void) {
+    if (this.events[event]?.push(callback) === undefined) {
+      this.events[event] = [callback];
+    }
+  }
+
+  public on(event: EventType, callback: (data: EventData) => void) {
+    this.attachEvent(event, callback);
+  }
+
+  public off(event: EventType, callback: (data: EventData) => void) {
+    if (this.events[event]) {
+      this.events[event] = this.events[event]?.filter((c) => c !== callback);
+    }
+  }
+
+  public once(event: EventType, callback: (data: EventData) => void) {
+    const onceHandler = (data: EventData) => {
+      this.off(event, onceHandler);
+      callback(data);
+    };
+
+    this.attachEvent(event, onceHandler);
+  }
+
+  public dispatch(event: EventType, data: EventData) {
+    this.events[event]?.forEach((callback) => {
+      callback(data);
+    });
+  }
+}
