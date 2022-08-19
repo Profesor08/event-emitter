acaeb56e8fead75f7d435a1f59eda5a357de6a0f
Oleg Baltag <online7890@gmail.com>
2022-08-19 13:43:48 +0300

    restor any type for static members for compatibility

--- src/EventEmitterStatic.ts
+++ src/EventEmitterStatic.ts
@@ -1,26 +1,26 @@
 export class EventEmitterStatic {
   protected static events: {
-    [key in string]?: ((data: void) => void)[];
+    [key in string]?: ((data: any) => void)[];
   } = {};
 
-  private static attachEvent(event: string, callback: (data: void) => void) {
+  private static attachEvent(event: string, callback: (data: any) => void) {
     if (this.events[event]?.push(callback) === undefined) {
       this.events[event] = [callback];
     }
   }
 
-  public static on(event: string, callback: (data: void) => void) {
+  public static on(event: string, callback: (data: any) => void) {
     this.attachEvent(event, callback);
   }
 
-  public static off(event: string, callback: (data: void) => void) {
+  public static off(event: string, callback: (data: any) => void) {
     if (this.events[event]) {
       this.events[event] = this.events[event]?.filter((c) => c !== callback);
     }
   }
 
-  public static once(event: string, callback: (data: void) => void) {
-    const onceHandler = (data: void) => {
+  public static once(event: string, callback: (data: any) => void) {
+    const onceHandler = (data: any) => {
       this.off(event, onceHandler);
       callback(data);
     };
@@ -28,7 +28,7 @@ export class EventEmitterStatic {
     this.attachEvent(event, onceHandler);
   }
 
-  public static dispatch(event: string, data: void) {
+  public static dispatch(event: string, data: any) {
     this.events[event]?.forEach((callback) => {
       callback(data);
     });
